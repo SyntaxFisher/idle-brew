fa0e855426fe0d0ab95ca5ea624d1e8c0587ae15
Jona Bihl <jona.bihl@tchibo.de>
2025-08-21 18:02:58 +0200

    feat: added KeyboardInterrupt handling

--- script.py
+++ script.py
@@ -14,12 +14,16 @@ start_time = datetime.now()
 end_time = start_time + timedelta(minutes=duration)
 print(f"Running for {duration} minutes, end time: {end_time.strftime('%H:%M')}")
 
-for i in range(1, total_seconds + 1):
-    print(f"\rProgress: {i}/{total_seconds} seconds", end="")
-    time.sleep(1)
-    pyautogui.press("space")
-    pyautogui.press("backspace")
+try:
+    for i in range(1, total_seconds + 1):
+        print(f"\rProgress: {i}/{total_seconds} seconds", end="")
+        time.sleep(1)
+        pyautogui.press("space")
+        pyautogui.press("backspace")
+    print()
+except KeyboardInterrupt:
+    print("\nSTOPPED BY USER")
 
 current_time = datetime.now().strftime("%H:%M")
-print(f"\nScript finished at {current_time}")
+print(f"Script finished at {current_time}")
 print("Exiting...")
